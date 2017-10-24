94077b0c49ae0e23b6b46c7678cbe24195f944b7
AHMET ÖZYILMAZ <aozyilmaz35@gmail.com>
2017-10-25 00:45:53 +0300

    updated part1

--- Hw1/AsmPart/Homework1.s
+++ Hw1/AsmPart/Homework1.s
@@ -1,55 +1,58 @@
 	.data
-buffer: .space 80
+MyArr: .space 80
     msg1:   .asciiz "Enter As string like NN.NN O NN.NN :   "
     str2:  .asciiz "You wrote:\n"
     str3:  .asciiz "input size is  :\n"
     newline:   .asciiz  "\n"
 	.text
-main:
+main: # DoCalculation
     jal takeString #take input from user 
     jal printInputSize # print input	
-	
-	la $9,buffer      #  address of input
-   	lb $8, 0($9)      #  ilk adresdeki byte ı al
-    move $10, $8 # adress 8 deki sayıyı  10.adrese ata
-  	sub $10,$10,0x2D # - ilk eleman - ise negatiftir ise
-  	beq $10,$0 add_SR
-    move $10, $8 # adress 8 deki sayıyı  10.adrese ata
-  	sub $10,$10,0x2D # - ise #constrol is first elemen is negative
-  	beq $10,$0 IsNegative1 # değilse ilk sayının int ve float kısımkarını ve sayılarını size bilgilerini ve ve kendini alıcaz
-    jal 1NDP # ilk sayının decimal partını alıcak
+    move $11,$8    #   size of input ;
+    li   $12,0    #   int number1Decimal = 0;
+    li   $13,0    #   int number1Float = 0;
+    li   $14,0    #   int number2Decimal = 0;
+    li   $15,0    #   int number2Float = 0;
+    li   $16,0    #   int number1IsPozitif = 0;
+    li   $17,0    #   int number2IsPozitif = 0;
+    li   $18,0    #   int resultIsPozitif = 0;
+    li   $19,0    #   int number1DecimalSize = 0;
+    li   $20,0    #   int number1FloatSize = 0;
+    li   $21,0    #   int number2DecimalSize = 0;
+    li   $22,0    #   int number2FloatSize = 0;
+    li   $23,0    #   int resultDecimal = 0;
+    li   $24,0    #   int resultFloat = 0;
+    li   $25,0    #   int resultDecimalSize = 0;
+    li   $26,0    #   int resultFloatSize = 0;
+    li   $27,0    #   index
+    li   $30,0    #   control
 
- 
+    la $9,buffer      #  address of input
+    lb $8, 0($9)      #  ilk adresdeki byte ı al
+    jal  IsDigit  # $8 == a[index];
+    add  $10,$10,1 # temp
+    sub  $10,$10,1 # temp  
+Halt:
     # Exit
     li  $v0,10      # exit
     syscall
 
-1NDP:#first number decimal part
-    lb $8, 0($9)      #  ilk adresdeki byte ı al
-    move $10, $8 # index deki elemanı al
-    jal IsDigit #   $18 == is digit se 1 değilse 0 olucak
-    bgt $18,1
-
+IsDigit: # if digit control == 1 else control == 0
+    move $10,$8  # $10 = a[index]
+    sub  $10,$10,0x30 # a[index] -= '0'
+    bge  $10,$0 IsDigit_J # if (t >= 0)
+    move $0,$30 return 0;
     jr $ra          # Jump to addr stored in $ra
-IsDigitSe: #ilk elemanı 10 kere topla sonra ikinci elemanla topla
-   
-Sum10Times:
-    
-    j 1NDP         # Jump to addr stored in $ra
-
-IsDigit: # $10 adress
-    sub $10,$10,0x30 # array[0]-=0 
-    ble $10,$0
-    
+IsDigit_J:    
+    sub $10,$0,$10  #t *= -1;
+    add $10,$10,9   #t += 9;
+    bge  $10,$0 IsDigit_J1 #  if (t >= 0)
+    jr $ra          # Jump to addr stored in $ra
+IsDigit_J1:
+    move $30,1  # $30 = 1 ;
     jr $ra          # Jump to addr stored in $ra
 
-NotNegative1:
-    ori $16,$0,0
-    jr $ra 
-IsNegative1:
-    ori $16,$0,1
-    jr $ra          # Jump to addr stored in $ra
-
+CheckOperator:
 
 add_SR:
 
@@ -57,10 +60,11 @@ substract_SR:
 
 multiply_SR:
 
+
 #end main
 printInputSize:
     ori      $8,$0,0        #  count = 0
-    la       $9,buffer      #  point at first c
+    la       $9,MyArr      #  point at first c
 loop:   
     lbu      $10,0($9)      # get the char
     sll      $0,$0,0        # branch delay
@@ -100,7 +104,7 @@ takeString:
 
     li $v0, 8       # take in input
 
-    la $a0, buffer  # load byte space into address
+    la $a0, MyArr  # load byte space into address
     li $a1, 20      # allot the byte space for string
 
     move $t0, $a0   # save string to t0
@@ -110,7 +114,7 @@ takeString:
     li $v0, 4
     syscall
 
-    la $a0, buffer  # reload byte space to primary address
+    la $a0, MyArr  # reload byte space to primary address
     move $a0, $t0   # primary address = t0 address (load pointer)
     li $v0, 4       # print string
     syscall
--- Hw1/main.cpp
+++ Hw1/main.cpp
@@ -1,48 +1,72 @@
 #include <iostream>
 #include <cstring>
+#include <cmath>
 
 using namespace std;
+
+int number1Decimal = 0;
+int number1Float = 0;
+int number2Decimal = 0;
+int number2Float = 0;
+
+int number1IsPozitif = 0;
+int number2IsPozitif = 0;
+int resultIsPozitif = 0;
+
+
+int number1DecimalSize = 0;
+int number1FloatSize = 0;
+int number2DecimalSize = 0;
+int number2FloatSize = 0;
+
+int resultDecimal = 0;
+int resultFloat = 0;
+int resultDecimalSize = 0;
+int resultFloatSize = 0;
 int IsDigit(char t);
 int CheckOperator(char t);
-double ADD();
-double SUB();
-double MULT();
+void ADD();
+void SUB();
+void MULT();
 void DoCalculation(char * a)
 {
-	int number1Decimal= 0 ;
-	int number1Float = 0;
-	int number2Decimal = 0;
-	int number2Float = 0;
-	int number1IsPozitif = 0;
-	int number2IsPositif = 0;
-	int number1DecimalSize = 0;
-	int number1FloatSize = 0;
-	int number2DecimalSize = 0;
-	int number2FloatSize = 0;
+	 number1Decimal = 0;
+	 number1Float = 0;
+	 number2Decimal = 0;
+	 number2Float = 0;
 
-	int resultDecimal = 0;
-	int resultFloat = 0;
-	int resultDecimalSize = 0;
-	int resultFloatSize = 0;
+	 number1IsPozitif = 0;
+	 number2IsPozitif = 0;
+	 resultIsPozitif = 0;
+
+
+	 number1DecimalSize = 0;
+	 number1FloatSize = 0;
+	 number2DecimalSize = 0;
+	 number2FloatSize = 0;
+
+	 resultDecimal = 0;
+	 resultFloat = 0;
+	 resultDecimalSize = 0;
+	 resultFloatSize = 0;
 
 	int size = strlen(a);
 	int control = 0;
+	int i = 0;
 
-	control = IsDigit(a[0]);
+	control = IsDigit(a[i]);
 
-	if (control == 0)
+	if (control == 0) { // sayý deðil
 		number1IsPozitif = 1; //number1 < 0;
-	else {
-		number1Decimal = a[0] - 48;
-		++number1DecimalSize;
+		++i;
 	}
-	int i = 1;
-	for (i = 1; i < size; ++i) {
+
+	for (; i < size; ++i) {
 		control = IsDigit(a[i]);
-		if (control == 1)
+		if (control == 1) // sayý ise 
 		{
 			number1Decimal *= 10;
-			number1Decimal += a[1] - 48 ;
+			number1Decimal += a[i] - 48 ;
 			++number1DecimalSize;
 		}
 		else 
@@ -63,22 +87,24 @@ void DoCalculation(char * a)
 		else
 			break;
 	}
+	
+	++i; //operatore geldi
 
-
-	++i; //boþluk
 	char k = a[i]; // hangi opertor olduðunu sakla 
 	++i;//boþluk
 	++i;
 
-	control = IsDigit(a[i]);
 
-	if (control == 0)
-		number2IsPositif = 1; //number2 < 0;
-	else {
-		number2Decimal = a[i] - 48;
-		++number2DecimalSize;
+	control = IsDigit(a[i]);
+	cout << "**** K *--->" << k << endl;
+	cout << "**** a[i] *--->" << a[i] << endl;
+	cout << "**** control *--->" << control << endl;
+
+	if (control == 0) { // is digit deðil 
+		number2IsPozitif = 1; //number2 < 0;
+		++i;
 	}
-	++i;
+	
 	for (; i < size; ++i) {
 		control = IsDigit(a[i]);
 		if (control == 1)
@@ -86,7 +112,6 @@ void DoCalculation(char * a)
 			number2Decimal *= 10;
 			number2Decimal += a[i] - 48;
 			++number2DecimalSize;
-
 		}
 		else
 			break;
@@ -104,36 +129,208 @@ void DoCalculation(char * a)
 			break;
 	}
 
+	if (number1FloatSize > number2FloatSize)
+	{
+		number2Float *= pow(10, number1FloatSize - number2FloatSize);
+		number2FloatSize = number1FloatSize;
+	}
+	else if (number2FloatSize > number1FloatSize)
+	{
+		number1Float *= pow(10, number2FloatSize - number1FloatSize);
+		number1FloatSize = number2FloatSize;
+	}
+
 	control = CheckOperator(k);
-	double result;
+	
 	if (control == 0) {
-		result=ADD();
+		ADD();
 	}
 	else if (control == 1) {
-		result = SUB();
-	}
+		SUB();
+	}/*
 	else if (control == 2) {
-		result = MULT();
+		MULT();
+	}
+	*/
+	cout << endl << "-->" << k << endl;
+
+	if (number1IsPozitif == 0)
+		cout << endl  << "-->" << number1DecimalSize << "  " << number1FloatSize << "  " << number1Decimal << "." << number1Float << endl;
+	else
+		cout << endl << "-->" << number1DecimalSize << "  " << number1FloatSize << "  -" << number1Decimal << "." << number1Float << endl;
+
+
+	if (number2IsPozitif == 0)
+		cout << endl << "-->" << number2DecimalSize << "  " << number2FloatSize << "  " << number2Decimal << "." << number2Float << endl;
+	else
+		cout << endl << "-->" << number2DecimalSize << "  " << number2FloatSize << "  -" << number2Decimal << "." << number2Float << endl;
+}
+void  ADD() {
+	if (number1IsPozitif == 0 && number2IsPozitif == 0) {
+		// + + +
+		// float kýsmýný topla
+		resultFloat = number1Float + number2Float;
+		int check = resultFloat;
+		for (size_t i = 0; i < number1Float; i++)
+		{
+			check /= 10;
+		}
+		resultFloatSize = number1FloatSize;
+
+		if (check > 1) {
+			resultDecimal++;
+		}
+		resultDecimal = number1Decimal + number2Decimal;
+
+		int temp = resultDecimal;
+		for (; temp > 0;) {
+			++resultDecimalSize;
+			temp /= 10;
+		}
+
+		resultIsPozitif = 0;
+		/* eðer result float ýn size ý number 1 dem büyükse resultDecimal i  1 arttýr */
+		//decimal kýsmý toplama
+	}
+	else if (number1IsPozitif == 0 && number2IsPozitif == 1) {
+		// + + -
+
+		resultFloat = number1Float - number2Float;
+		resultDecimal = number1Decimal - number2Decimal;
+		/*cout << "DECIMAL" << resultDecimal << endl;
+		cout << "DECIMAL" << number1Decimal << endl;
+		cout << "DECIMAL" << number2Decimal << endl;*/
+
+		if (resultFloat < 0) {
+			if (resultDecimal > 0) {
+				resultFloat *= -1;
+				resultDecimal--;
+				resultFloat += pow(10, number1FloatSize);
+			}
+		}
+		resultFloatSize = number1FloatSize;
+		int temp = resultDecimal;
+
+		for (; temp > 0;) {
+			temp /= 10;
+			++resultDecimalSize;
+		}
+		// size fonksiyonun çaðýr resultDecimal size a ata
+		if (resultDecimal < 0)
+			resultIsPozitif = 1; //result < 0
+		else
+			resultIsPozitif = 0; //result > 0
+
+	}
+	else if (number1IsPozitif == 1 && number2IsPozitif == 0) {
+		// - + +
+		resultFloat = number2Float - number1Float;
+		if (resultFloat < 0) {
+			resultFloat *= -1;
+			if (resultDecimal > 0) {
+				resultDecimal--;
+				resultFloat += pow(10, number2FloatSize);
+			}
+
+			resultFloatSize = number2FloatSize;
+			resultDecimal = number2Decimal - number1Decimal;
+			// size fonksiyonun çaðýr resultDecimal size a ata
+			if (resultDecimal < 0)
+				resultIsPozitif = 1; //result < 0
+			else
+				resultIsPozitif = 0; //result > 0
+
+		}
+
+		//decimal
+	}
+	else if (number1IsPozitif == 1  && number2IsPozitif == 1) {
+		// - + -,
+		resultFloat = number1Float + number2Float;
+		int check = resultFloat;
+		for (size_t i = 0; i < number1Float; i++)
+		{
+			check /= 10;
+		}
+		resultFloatSize = number1FloatSize;
+
+		if (check > 1) {
+			resultDecimal++;
+		}
+		resultDecimal = number1Decimal + number2Decimal;
+
+		int temp = resultDecimal;
+		for (; temp > 0;) {
+			++resultDecimalSize;
+			temp /= 10;
+		}
+
+		resultIsPozitif = 1;
 	}
 
-	cout << "-->" << number1DecimalSize << "  " << number1FloatSize << "  " << number1Decimal << "." << number1Float << endl;
-	cout << "-->" << k << endl;
-	cout << "-->" << number2DecimalSize << "  " << number2FloatSize << "  "<< number2Decimal<<"."<< number2Float << endl;
+}
+void SUB() {
 
+	if (number1IsPozitif == 0 && number2IsPozitif == 0) {
+		// + - + 
+		number2IsPozitif = 1; // number 2 negatif gibi davransýn 
+		ADD();
+		number2IsPozitif = 0; // number 2 negatif gibi davransýn 
 
-	cout << result;
+	}
+	else if (number1IsPozitif == 0 && number2IsPozitif ==1 ) {
+		// + - -
+		number2IsPozitif = 0; // number 2 pozitif gibi davransýn
+		ADD();
+		number2IsPozitif = 1; // number 2 negatif gibi davransýn 
+
+	}
+	else if (number1IsPozitif == 1 && number2IsPozitif == 0) {
+		// - - +
+		number2IsPozitif = 1; // number 2 negatif gibi davransýn 
+		ADD();
+		number2IsPozitif = 0; // number 2 negatif gibi davransýn 
+
+	}
+	else if (number1IsPozitif == 1 && number2IsPozitif == 1) {
+		// - - -
+		number2IsPozitif = 0; // number 2 negatif gibi davransýn
+		ADD();
+		number2IsPozitif = 1; // number 2 negatif gibi davransýn
+
+	}
 }
-double  ADD() {
-	double result=.0;
-	return result;
-}
-double SUB() {
-	double result = .0;
-	return result;
-}
-double MULT() {
-	double result = .0;
-	return result;
+void MULT() {
+
+	if (number1IsPozitif >= 0 && number2IsPozitif >= 0) {
+		// + * + 
+		// döngü içinde tolasýn sonuç  + 
+		resultIsPozitif = 0; // result > 0 
+	}
+	else if (number1IsPozitif >= 0 && number2IsPozitif < 0) {
+		// + * -
+		resultIsPozitif = 1; // result < 0 
+	}
+	else if (number1IsPozitif < 0 && number2IsPozitif >= 0) {
+		// - * +
+		resultIsPozitif = 1; // result < 0 
+	}
+	else if (number1IsPozitif < 0 && number2IsPozitif < 0) {
+		// - * -
+		resultIsPozitif = 0; // result > 0 
+	}
+
+	int f = 0;
+	int mul = 1;
+	for (int i = 0; i < number2FloatSize; ++i) {
+		f = number2Float % 10;
+		f *= mul;
+		for (int h = 0; h < f; h++)
+		{
+			resultFloat += number1Float;
+		}
+		mul *= 10;
+	}
 }
 int CheckOperator(char t) {
 	if (t == '+')
@@ -145,21 +342,44 @@ int CheckOperator(char t) {
 	return -1;
 }
 int IsDigit(char t) {
-	t -= '0';
-	if (t > 0) {
+	t -= 48;
+	if (t >= 0) {
 		t *= -1;
 		t += 9;
-		if (t > 0)
+		if (t >= 0)
 			return 1;
 	}
-
 	return 0;
 }
 
 int main() {
-	DoCalculation("12.456 + 412.1234");
+	DoCalculation("12.456 - 412.12");
 
-	int a = 0;
+	if(resultIsPozitif == 0)
+		cout << endl << resultDecimal << " . " << resultFloat << endl;
+	else
+		cout << endl <<"-" <<resultDecimal << " . " << resultFloat<<endl;
+
+	DoCalculation("-6782.456 - 412 .12");
+
+	if (resultIsPozitif == 0)
+		cout << endl << resultDecimal << " . " << resultFloat;
+	else
+		cout << endl << "-" << resultDecimal << " . " << resultFloat << endl;
+
+	DoCalculation("-178.456 - -412.1234");
+
+	if (resultIsPozitif == 0)
+		cout << endl << resultDecimal << " . " << resultFloat << endl;
+	else
+		cout << endl << "-" << resultDecimal << " . " << resultFloat << endl;
+	
+	DoCalculation("11232.456 - -412.14");
+
+	if (resultIsPozitif == 0)
+		cout << endl << resultDecimal << " . " << resultFloat << endl;
+	else
+		cout << endl  << "-" << resultDecimal << " . " << resultFloat << endl;	int a = 0;
 	cin >> a;
 	return 0;
 }
